1ee1830d991d7b86bb5bbcc14ffc6dc2d0b1872d
shijie.chen_c <shijie.chen_c@chinapnr.com>
2019-01-25 13:40:11 +0800

    commit the test class

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+namespace Chenginus\TestOne;
+
+class Test
+{
+    public function __construct()
+    {
+    }
+    
+    /**
+     * This is my first package demo.
+     *
+     * @author chenginus
+     * @return string
+     */
+    public function test()
+    {
+        return 'Congratulations !';
+    }
+}
